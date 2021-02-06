c7c1aca7c9f36b9832e0b8499fdee5a191ddcdf3
mamoussa <mamoussa@vermeg.com>
2021-02-06 21:59:43 +0100

    update db connection

--- app/models/database.js
+++ app/models/database.js
@@ -2,11 +2,11 @@ var Sequelize = require('sequelize');
 
 
 const sequelize = new Sequelize(
-  '99.000webhost.io', // database
+  'id16100695_moc', // database
   'id16100695_moussaoptique', // user / usuario
   '%Gtr0uI0YRB90DkZ', //password
   {
-    host: 'localhost',
+    host: '99.000webhost.io',
     dialect: 'mysql'
     ,
     
