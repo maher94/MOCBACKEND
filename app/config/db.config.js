16f2970fbda09e55810ca72d19f383222f10648c
mamoussa <mamoussa@vermeg.com>
2021-02-06 22:21:20 +0100

    update db connection

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "databases.000webhost.com",
+  HOST: "www.000webhost.com",
   USER: "id16100695_moussaoptique",
   PASSWORD: "%Gtr0uI0YRB90DkZ",
   DB: "id16100695_moc",
--- app/models/database.js
+++ app/models/database.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   'id16100695_moussaoptique', // user / usuario
   '%Gtr0uI0YRB90DkZ', //password
   {
-    host: 'databases.000webhost.com',
+    host: 'www.000webhost.com',
     dialect: 'mysql'
     ,
     
