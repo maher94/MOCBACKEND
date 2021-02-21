63901ce32dbfcf6f7b6b915bd9a3cc1b0b56ce52
mamoussa <mamoussa@vermeg.com>
2021-02-21 10:58:48 +0100

    update app

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.use('/test', (req, res) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
